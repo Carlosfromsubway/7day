9ba1191b1e0cae7015040087f755386485084dda
Carlosfromsubway <tlee285@my.bcit.ca>
2023-03-04 15:28:56 -0800

    s

--- pages/index.js
+++ pages/index.js
@@ -109,13 +109,14 @@ export default function Home() {
 
 useEffect(() => {
   //if(grabWeather.current === true){
-    fetchWeather();
+   
   //}
 
   //return () => {
     //grabWeather.current = true;
  // }
-}, []);
+ fetchWeather()
+}, [])
 
 const current = new Date();
 const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
